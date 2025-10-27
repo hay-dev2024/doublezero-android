f8a89f36c489b36ca0f7478b014140f69681a891
hay-dev2024 <hay.dev2024@gmail.com>
2025-10-27 15:36:40 +0900

    feat: add hilt

--- app/src/main/java/com/doublezero/app/MainActivity.kt
+++ app/src/main/java/com/doublezero/app/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.doublezero.app.ui.theme.DoubleZeroTheme
 import com.doublezero.feature_home.HomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +21,7 @@ class MainActivity : ComponentActivity() {
             DoubleZeroTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     HomeScreen(
-                        onNavigate = { route -> /* TODO: 실제 네비게이션 처리 */ },
+                        onNavigate = { /* TODO: 실제 네비게이션 처리 */ },
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
