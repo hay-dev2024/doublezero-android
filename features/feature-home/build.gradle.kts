5fe4377596a449a68217af50ea70f4a044ccc058
hay <hay.dev2024@gmail.com>
2025-12-03 11:33:39 +0900

    build: add google maps heatmap library

--- features/feature-home/build.gradle.kts
+++ features/feature-home/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:19.2.0")
     implementation("com.google.maps.android:maps-compose:6.12.2")
     implementation("com.google.maps.android:maps-utils-ktx:5.2.1")
+    // Google Maps Utils for heatmap
+    implementation("com.google.maps.android:android-maps-utils:3.19.1")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
